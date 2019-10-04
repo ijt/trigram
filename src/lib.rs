3bca9ee85eaf920d0398363e48caa1d45266a0d2
Issac Trotts <issac.trotts@gmail.com>
2019-10-04 15:46:06 -0700

    fix up treatment of multiple words, skip over punctuation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /*! 
-The trigram library computes the similarity of strings. The idea is to do
-something like the similarity function in the Postgresql pg_trgm extension:
+The trigram library computes the similarity of strings, inspired by the similarity function in the
+Postgresql pg_trgm extension:
 https://www.postgresql.org/docs/9.1/pgtrgm.html.
 */
 
@@ -18,6 +18,27 @@ pub fn similarity(a: &String, b: &String) -> f32 {
 }
 
 pub fn trigrams(s: &String) -> HashSet<String> {
+    let mut ts: HashSet<String> = HashSet::new();
+    for w in words(s) {
+        for tw in trigrams_for_word(&w) {
+            ts.insert(tw);
+        }
+    }
+    ts
+}
+
+pub fn jaccard<T>(s1: HashSet<T>, s2: HashSet<T>) -> f32 where T: Hash+Eq {
+    let i = s1.intersection(&s2).count() as f32;
+    let u = s1.union(&s2).count() as f32;
+    if u == 0.0 { 1.0 } else { i / u }
+}
+
+fn words(s: &String) -> Vec<String> {
+    let rx = regex::Regex::new(r"\w+").unwrap();
+    rx.find_iter(s).map(|m| m.as_str().to_string()).collect()
+}
+
+fn trigrams_for_word(s: &String) -> HashSet<String> {
     let mut ts = HashSet::new();
     let s = format!("{} ", s);
     let mut p1 = ' ';
@@ -30,12 +51,6 @@ pub fn trigrams(s: &String) -> HashSet<String> {
         p2 = c;
     }
     ts
-}
-
-pub fn jaccard<T>(s1: HashSet<T>, s2: HashSet<T>) -> f32 where T: Hash+Eq {
-    let i = s1.intersection(&s2).count() as f32;
-    let u = s1.union(&s2).count() as f32;
-    return i / u;
 }
 
 #[cfg(test)]
@@ -67,10 +82,12 @@ mod tests {
 
     #[test]
     fn fuzzy_matches() {
-        // Check for agreement with the answers given by the postgres pg_trgm similarity function.
+        // Check for agreement with answers given by the postgres pg_trgm similarity function.
         assert_eq!(similarity(&"a".to_string(), &"ab".to_string()), 0.25, "checking a and ab");
-        assert_eq!(similarity(&"dancing bear".to_string(), &"dancing boar".to_string()), 0.625, "checking dancing bear and dancing boar");
         assert_eq!(similarity(&"foo".to_string(), &"food".to_string()), 0.5, "checking foo and food");
         assert_eq!(similarity(&"bar".to_string(), &"barred".to_string()), 0.375, "checking bar and barred");
+        assert_eq!(similarity(&"dancing bear".to_string(), &"dancing boar".to_string()), 0.625, "checking dancing bear and dancing boar");
+        assert_eq!(similarity(&"sir sly".to_string(), &"srsly".to_string()), 0.3, "checking sir sly and srsly");
+        assert_eq!(similarity(&"same, but different?".to_string(), &"same but different".to_string()), 1.0, "checking same but different");
     }
 }
