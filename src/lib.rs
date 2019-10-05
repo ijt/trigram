7ddb1d9cf08bfb535b3c65f0dc76f1161284dd4c
Issac Trotts <issac.trotts@gmail.com>
2019-10-04 20:27:15 -0700

    change from String to &str for a ~30x speedup
    The benchmark went from 1,360,345 to 43,410 ns/iter.

--- src/lib.rs
+++ src/lib.rs
@@ -7,33 +7,32 @@ https://www.postgresql.org/docs/9.1/pgtrgm.html.
 #![feature(test)]
 
 extern crate test;
+#[macro_use] extern crate lazy_static;
 
 use std::collections::HashSet;
 use std::hash::Hash;
-
-/// Distance between two strings as 1 - their similarity. This is not a metric because different
-/// strings can have a distance of zero since differences in whitespace and punctuation are
-/// ignored. For example `distance("a".to_string(), "a!".to_string())` is 0.0.
-pub fn distance(a: &String, b: &String) -> f32 {
-    1.0 - similarity(a, b)
-}
+use regex::Regex;
 
 /// Similarity of two strings as the Jaccard similarity of their trigram sets.
-pub fn similarity(a: &String, b: &String) -> f32 {
-    let ta = trigrams(a);
-    let tb = trigrams(b);
+pub fn similarity(a: &str, b: &str) -> f32 {
+    lazy_static! {
+        static ref RX: Regex = Regex::new(r"^|$|\W+").unwrap();
+    }
+    let a = RX.replace_all(a, "  ");
+    let b = RX.replace_all(b, "  ");
+    let ta = trigrams(&a[0..a.len()]);
+    let tb = trigrams(&b[0..b.len()]);
+    println!("trigrams of a: {:?}, trigrams of b: {:?}", sorted(&ta), sorted(&tb));
     return jaccard(ta, tb);
 }
 
-/// Set of character trigrams of the words in the input string.
-pub fn trigrams(s: &String) -> HashSet<String> {
-    let mut ts: HashSet<String> = HashSet::new();
-    for w in words(s) {
-        for tw in trigrams_for_word(&w) {
-            ts.insert(tw);
-        }
+fn sorted<'a>(hs: &HashSet<&'a str>) -> Vec<&'a str> {
+    let mut v: Vec<&str> = Vec::new();
+    for s in hs {
+        v.push(s);
     }
-    ts
+    v.sort();
+    v
 }
 
 /// Jaccard similarity between two sets.
@@ -44,22 +43,16 @@ pub fn jaccard<T>(s1: HashSet<T>, s2: HashSet<T>) -> f32 where T: Hash+Eq {
     if u == 0.0 { 1.0 } else { i / u }
 }
 
-fn words(s: &String) -> Vec<String> {
-    let rx = regex::Regex::new(r"\w+").unwrap();
-    rx.find_iter(s).map(|m| m.as_str().to_string()).collect()
-}
-
-fn trigrams_for_word(s: &String) -> HashSet<String> {
+fn trigrams(s: &str) -> HashSet<&str> {
     let mut ts = HashSet::new();
-    let s = format!("{} ", s);
-    let mut p1 = ' ';
-    let mut p2 = ' ';
-    for c in s.chars() {
-        let v = vec![p1, p2, c];
-        let t: String = v.into_iter().collect();
-        ts.insert(t);
-        p1 = p2;
-        p2 = c;
+    if s.len() < 3 {
+        return ts
+    }
+    for i in 0..s.len()-3 {
+        let t = &s[i..i+3];
+        if &t[1..3] != "  " {
+            ts.insert(t);
+        }
     }
     ts
 }
@@ -68,6 +61,9 @@ fn trigrams_for_word(s: &String) -> HashSet<String> {
 mod tests {
     use super::*;
     use test::Bencher;
+
+    #[test]
+    fn empty() { assert_eq!(similarity(&"", &""), 1.0, "checking similarity of '' to ''"); }
 
     #[test]
     fn same_string() {
@@ -98,6 +94,7 @@ mod tests {
         assert_eq!(similarity(&"a".to_string(), &"ab".to_string()), 0.25, "checking a and ab");
         assert_eq!(similarity(&"foo".to_string(), &"food".to_string()), 0.5, "checking foo and food");
         assert_eq!(similarity(&"bar".to_string(), &"barred".to_string()), 0.375, "checking bar and barred");
+        assert_eq!(similarity(&"ing bear".to_string(), &"ing boar".to_string()), 0.5, "checking ing bear and ing boar");
         assert_eq!(similarity(&"dancing bear".to_string(), &"dancing boar".to_string()), 0.625, "checking dancing bear and dancing boar");
         assert_eq!(similarity(&"sir sly".to_string(), &"srsly".to_string()), 0.3, "checking sir sly and srsly");
         assert_eq!(similarity(&"same, but different?".to_string(), &"same but different".to_string()), 1.0, "checking same but different");
